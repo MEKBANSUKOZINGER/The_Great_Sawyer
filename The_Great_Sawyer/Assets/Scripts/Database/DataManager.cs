e9d9598e870ad9160135d68df679e93bb07e8af1
Ozinger <chocosdj@naver.com>
2024-02-03 23:03:58 +0900

    [#4] Json DB System Implemented.
    Edited class for json conversion.
    Json Save / Load function implemented using jsonUtility.
    File path is C:/Users/(username)/Appdata/LocalLow/(companyName)/(projectName)/~~~.json.

--- The_Great_Sawyer/Assets/Scripts/Database/DataManager.cs
+++ The_Great_Sawyer/Assets/Scripts/Database/DataManager.cs
@@ -1,30 +1,26 @@
+using Newtonsoft.Json.Bson;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using UnityEditor.UIElements;
 using UnityEngine;
-using Newtonsoft.Json;
 
 public class DataManager : MonoBehaviour
 {
+    //Singletone
     public static DataManager Instance;
-    public class City
-    {
-        public string cityName;
-        public bool isUnlocked;
 
-        public City()
-        {
-            cityName = "temp";
-            isUnlocked = false;
-        }
-    }
+    private string userFilePath;
+    private string itemsFilePath;
+    private string furnituresFilePath;
+    private string questsFilePath;
+    private string achievementsFilePath;
 
-    public class Cities
-    {
-        public City[] cities;
-    }
+    //Classes
 
+    [Serializable]
     public class Item
     {
         public string name;
@@ -35,14 +31,22 @@ public class DataManager : MonoBehaviour
             name = "temp";
             description = "This is description";
         }
+
+        public Item(string _name, string _description)
+        {
+            name = _name;
+            description = _description;
+        }
     }
 
+    [Serializable]
     public class Items
     {
         public Item[] items;
     }
 
-    public class  Furniture
+    [Serializable]
+    public class Furniture
     {
         public string furnitureName;
         public string description;
@@ -64,13 +68,27 @@ public class DataManager : MonoBehaviour
             rotate = 0;
             batchTilemap = "tilemap";
         }
+
+        public Furniture(string _name, string _description)
+        {
+            furnitureName = _name;
+            description = _description;
+            color = "red";
+            isBatched = false;
+            batchX = 0;
+            batchY = 0;
+            rotate = 0;
+            batchTilemap = "None";
+        }
     }
 
-    public class furnitures
+    [Serializable]
+    public class Furnitures
     {
         public Furniture[] furnitureList;
     }
 
+    [Serializable]
     public class DailyQuest
     {
         public string name;
@@ -86,13 +104,25 @@ public class DataManager : MonoBehaviour
             name = "tempQuest";
             description = "This is quest";
             requireItem = "tempItem";
-            requireAmount = 1; 
+            requireAmount = 1;
             isCleared = false;
             rewardMoney = "ironIngot";
             rewardAmount = 10;
         }
+
+        public DailyQuest(string _name, string _description, string _requireItem, int _requireAmount, string _rewardMoney, int _rewardAmount)
+        {
+            name = _name;
+            description = _description;
+            requireItem = _requireItem;
+            requireAmount = _requireAmount;
+            rewardMoney = _rewardMoney;
+            rewardAmount = _rewardAmount;
+            isCleared = false;
+        }
     }
 
+    [Serializable]
     public class WeeklyQuest
     {
         public string name;
@@ -113,8 +143,20 @@ public class DataManager : MonoBehaviour
             rewardMoney = "ironIngot";
             rewardAmount = 10;
         }
+
+        public WeeklyQuest(string _name, string _description, string _requireItem, int _requireAmount, string _rewardMoney, int _rewardAmount)
+        {
+            name = _name;
+            description = _description;
+            requireItem = _requireItem;
+            requireAmount = _requireAmount;
+            rewardMoney = _rewardMoney;
+            rewardAmount = _rewardAmount;
+            isCleared = false;
+        }
     }
 
+    [Serializable]
     public class MonthlyQuest
     {
         public string name;
@@ -135,16 +177,31 @@ public class DataManager : MonoBehaviour
             rewardMoney = "ironIngot";
             rewardAmount = 10;
         }
+
+        public MonthlyQuest(string _name, string _description, string _requireItem, int _requireAmount, string _rewardMoney, int _rewardAmount)
+        {
+            name = _name;
+            description = _description;
+            requireItem = _requireItem;
+            requireAmount = _requireAmount;
+            rewardMoney = _rewardMoney;
+            rewardAmount = _rewardAmount;
+            isCleared = false;
+        }
     }
 
+    [Serializable]
     public class Quests
     {
         public DailyQuest[] dailyQuests;
         public WeeklyQuest[] weeklyQuests;
         public MonthlyQuest[] monthlyQuests;
+
+
     }
 
-    public class moneyAchivement
+    [Serializable]
+    public class moneyAchievement
     {
         public string name;
         public string description;
@@ -154,7 +211,7 @@ public class DataManager : MonoBehaviour
         public string rewardMoney;
         public int rewardAmount;
 
-        public moneyAchivement()
+        public moneyAchievement()
         {
             name = "tempMoneyAchivement";
             description = "description";
@@ -165,7 +222,8 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    public class  furnitureAchivement
+    [Serializable]
+    public class furnitureAchievement
     {
         public string name;
         public string description;
@@ -175,7 +233,7 @@ public class DataManager : MonoBehaviour
         public string rewardMoney;
         public int rewardAmount;
 
-        public furnitureAchivement()
+        public furnitureAchievement()
         {
             name = "tempMoneyAchivement";
             description = "description";
@@ -186,7 +244,8 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    public class levelAchivement
+    [Serializable]
+    public class levelAchievement
     {
         public string[] name;
         public string[] description;
@@ -197,7 +256,7 @@ public class DataManager : MonoBehaviour
         public string rewardVariable;
         public int[] rewardAmount;
 
-        public levelAchivement()
+        public levelAchievement()
         {
             name = new string[] { };
             description = new string[] { };
@@ -210,20 +269,24 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    public class Achivements
+    [Serializable]
+    public class Achievements
     {
-        public moneyAchivement[] moneyAchivements;
-        public furnitureAchivement[] furnitureAchivements;
-        public levelAchivement[] levelAchivements;
+        public moneyAchievement[] moneyAchivements;
+        public furnitureAchievement[] furnitureAchivements;
+        public levelAchievement[] levelAchivements;
     }
 
+    [Serializable]
     public class User
     {
         public string userName;
         public int birthDay;
         public int workDays;
-        public City currentCity;
+        public string currentCity;
         public Money money;
+
+        [Serializable]
         public class Money
         {
             public int ironIngot;
@@ -237,8 +300,8 @@ public class DataManager : MonoBehaviour
             public int ticket;
             public Money()
             {
-                ironIngot = 0;
-                gear = 0;  
+                ironIngot = 500;
+                gear = 0;
                 blueIce = 0;
                 star = 0;
                 shell = 0;
@@ -249,38 +312,216 @@ public class DataManager : MonoBehaviour
         }
         public Inventory inventory;
 
+        [Serializable]
         public class Inventory
         {
-            public Dictionary<Item, int> items;
-            public Dictionary<Furniture, int> furnitures;
+            public Item[] myItems;
+            public int[] itemAmounts;
+            public Furniture[] myFurnitures;
+            public int[] furnitureAmounts;
 
             public Inventory()
             {
-                items = new Dictionary<Item, int>();
-                furnitures = new Dictionary<Furniture, int>();
+                myItems = new Item[0];
+                itemAmounts = new int[0];
+                myFurnitures = new Furniture[0];
+                furnitureAmounts = new int[0];
             }
         }
 
-        public DailyQuest[] dailyQuests;
-        public WeeklyQuest[] weeklyQuests;
-        public MonthlyQuest[] monthlyQuests;
+        public Quests myQuest;
         public int clearedQuest;
-
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-
     }
 
-    // Update is called once per frame
-    void Update()
+    public User user;
+    public Items items;
+    public Furnitures furnitures;
+    public Quests quests;
+    public Achievements achievements;
+
+    //Functions
+    void Awake() //set file pathes.
     {
-        
+        userFilePath = Application.persistentDataPath + "/user.json";
+        itemsFilePath = Application.persistentDataPath + "/items.json";
+        furnituresFilePath = Application.persistentDataPath + "/furnitures.json";
+        questsFilePath = Application.persistentDataPath + "/quests.json";
+        achievementsFilePath = Application.persistentDataPath + "/achievements.json";
     }
 
-    public void SaveData()
+
+    public void SaveData(string type)
     {
-        //FileStream stream = new FileStream(Application.dataPath + "/Data")
+        switch (type)
+        {
+            case "user":
+                SaveUserData(); break;
+            case "items":
+                SaveItemsData(); break;
+            case "furnitures":
+                SaveFurnituresData(); break;
+            case "quests":
+                SaveQuestsData(); break;
+            case "achivements":
+                SaveAchivementsData(); break;
+            case "all":
+                SaveUserData();
+                SaveItemsData();
+                SaveFurnituresData();
+                SaveQuestsData();
+                SaveAchivementsData();
+                break;
+        }
+        Debug.Log(type + " Saved.");
+    }
+
+    private void SaveUserData()
+    {
+        string jsonStr = JsonUtility.ToJson(user, true);
+        File.WriteAllText(userFilePath, jsonStr);
+    }
+
+    private void SaveItemsData()
+    {
+        string jsonStr = JsonUtility.ToJson(items, true);
+        File.WriteAllText(itemsFilePath, jsonStr);
+    }
+
+    private void SaveFurnituresData()
+    {
+        string jsonStr = JsonUtility.ToJson(furnitures, true);
+        File.WriteAllText(furnituresFilePath, jsonStr);
+    }
+    private void SaveQuestsData()
+    {
+        string jsonStr = JsonUtility.ToJson(quests, true);
+        File.WriteAllText(questsFilePath, jsonStr);
+    }
+
+    private void SaveAchivementsData()
+    {
+        string jsonStr = JsonUtility.ToJson(achievements, true);
+        File.WriteAllText(achievementsFilePath, jsonStr);
+    }
+
+    public void LoadData(string type)
+    {
+        switch (type)
+        {
+            case "user":
+                LoadUserData(); break;
+            case "items":
+                LoadItemsData(); break;
+            case "furnitures":
+                LoadFurnituresData(); break;
+            case "quests":
+                LoadQuestsData(); break;
+            case "achievements":
+                LoadAchievementsData(); break;
+            case "all":
+                LoadUserData();
+                LoadItemsData();
+                LoadFurnituresData();
+                LoadQuestsData();
+                LoadAchievementsData();
+                break;
+        }
+        Debug.Log(type + " Loaded.");
+    }
+
+    private void LoadUserData()
+    {
+        if (!File.Exists(userFilePath))
+        {
+            Debug.Log("Load Failed");
+        } else
+        {
+            string jsonStr = File.ReadAllText(userFilePath);
+            user = JsonUtility.FromJson<User>(jsonStr);
+        }
+    }
+
+    private void LoadItemsData()
+    {
+        if (!File.Exists(itemsFilePath))
+        {
+            Debug.Log("Load Failed");
+        }
+        else
+        {
+            string jsonStr = File.ReadAllText(itemsFilePath);
+            items = JsonUtility.FromJson<Items>(jsonStr);
+        }
+    }
+
+    private void LoadFurnituresData()
+    {
+        if (!File.Exists(furnituresFilePath))
+        {
+            Debug.Log("Load Failed");
+        }
+        else
+        {
+            string jsonStr = File.ReadAllText(furnituresFilePath);
+            furnitures = JsonUtility.FromJson<Furnitures>(jsonStr);
+        }
+    }
+
+    private void LoadQuestsData()
+    {
+        if (!File.Exists(questsFilePath))
+        {
+            Debug.Log("Load Failed");
+        }
+        else
+        {
+            string jsonStr = File.ReadAllText(questsFilePath);
+            quests = JsonUtility.FromJson<Quests>(jsonStr);
+        }
+    }
+
+    private void LoadAchievementsData()
+    {
+        if (!File.Exists(achievementsFilePath))
+        {
+            Debug.Log("Load Failed");
+        }
+        else
+        {
+            string jsonStr = File.ReadAllText(achievementsFilePath);
+            achievements = JsonUtility.FromJson<Achievements>(jsonStr);
+        }
+    }
+
+    public void Init()
+    {
+        //Items init
+        Item apple = new Item("사과", "정말 단 사과입니다!");
+        Item banana = new Item("바나나", "정말 길쭉한 바나나입니다!");
+        items.items = new Item[] { apple, banana };
+
+        //Furnitures init
+        Furniture lamp = new Furniture("라바 램프", "슬라임이 들어있는 라바 램프입니다.");
+        Furniture closet = new Furniture("신비한 옷장", "여기 들어갔다 나오는 순간 나도 패셔니스타!");
+        furnitures.furnitureList = new Furniture[] { lamp, closet };
+
+        //Quests init
+        DailyQuest helpGrandma = new DailyQuest("할머니 도와드리기", "할머니를 도와 수레를 끌어주세요!", "목장갑", 2, "철조각", 50);
+        DailyQuest manageTrain = new DailyQuest("열차 정비하기", "열차에 이상은 없는지 점검해주세요!", "스패너", 1, "톱니바퀴", 10);
+        WeeklyQuest meetPeople = new WeeklyQuest("E의 면모 뽐내기", "새로운 NPC를 5명 만나 대화를 나누세요!", "None", 0, "톱니바퀴", 50);
+        WeeklyQuest adventrue = new WeeklyQuest("나는야 탐험가!", "5일간 프레드와 하루에 한번씩 대화하세요!", "None", 0, "철조각", 100);
+        MonthlyQuest newCities = new MonthlyQuest("나는야 개척자!", "다른 도시로 두 번 이상 이동하세요!", "긴급연료", 50, "교환권", 20);
+        MonthlyQuest hiddenNPC = new MonthlyQuest("...", "숨겨진 NPC와 대화하세요!", "휴대용 램프", 1, "철조각", 1000);
+        quests.dailyQuests = new DailyQuest[] { helpGrandma, manageTrain };
+        quests.weeklyQuests = new WeeklyQuest[] { meetPeople, adventrue };
+        quests.monthlyQuests = new MonthlyQuest[] { newCities, hiddenNPC };
+
+        SaveData("items");
+        SaveData("furnitures");
+        SaveData("quests");
+        //SaveData("user");
+
+        LoadData("user");
+        Debug.Log(user.userName);
     }
 }
